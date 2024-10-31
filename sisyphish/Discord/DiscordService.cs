c122429bccaf024b94fa5567e2cbf908fe33f0a9
Jacob <jacob.heft@gmail.com>
2024-10-30 20:49:47 -0400

    Extract DiscordService

---
+++ sisyphish/Discord/DiscordService.cs
@@ -0,0 +1,45 @@
+using Flurl.Http;
+using sisyphish.Discord.Models;
+
+namespace sisyphish.Discord;
+
+public class DiscordService : IDiscordService
+{
+    private readonly ILogger<DiscordService> _logger;
+
+    public DiscordService(ILogger<DiscordService> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task DeferResponse(DiscordInteraction interaction)
+    {
+        var deferral = new DiscordDeferralCallbackResponse();
+
+        using var httpClient = new HttpClient();
+        await httpClient.PostAsJsonAsync(
+            requestUri: $"{Config.DiscordBaseUrl}/interactions/{interaction.Id}/{interaction.Token}/callback",
+            value: deferral);
+    }
+
+    public async Task EditResponse(DiscordInteraction interaction, string content)
+    {
+        var body = new DiscordInteractionEdit
+        {
+            Content = content
+        };
+
+        var success = false;
+        var attempts = 0;
+        while (!success && attempts < 5)
+        {
+            var response = await $"{Config.DiscordBaseUrl}/webhooks/{Config.DiscordApplicationId}/{interaction.Token}/messages/@original"
+                .AllowAnyHttpStatus()
+                .PatchJsonAsync(body);
+            
+            _logger.LogInformation($"Response status code: {response.StatusCode}");
+
+            success = true;
+        }
+    }
+}
