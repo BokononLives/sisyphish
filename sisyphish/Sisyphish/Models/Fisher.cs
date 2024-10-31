24bdce9e9a203868673b9a8c226fde806364e792
Jacob <jacob.heft@gmail.com>
2024-10-30 20:07:42 -0400

    Simplify Fisher model

--- sisyphish/Sisyphish/Models/Fisher.cs
+++ sisyphish/Sisyphish/Models/Fisher.cs
@@ -8,9 +8,8 @@ public class Fisher
     [FirestoreProperty("id")] public string? Id { get; set; }
     [FirestoreProperty("created_at")] public DateTime? CreatedAt { get; set; }
     [FirestoreProperty("discord_user_id")] public string? DiscordUserId { get; set; }
-    [FirestoreProperty("fish_caught")] public List<Fish> Fish { get; set; } = [];
+    [FirestoreProperty("fish_caught")] public long? FishCaught { get; set; }
+    [FirestoreProperty("biggest_fish")] public long? BiggestFish { get; set; }
     [FirestoreProperty("locked_at")] public DateTime? LockedAt { get; set; }
-    public long? FishCaught => Fish.Count();
-    public long? BiggestFish => Fish.Max(f => f.Size);
     public DateTime? LastUpdated { get; set; }
 }
