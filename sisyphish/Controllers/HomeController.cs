96b03baba04a305c4c1b51c325b0655b7c56190a
Jacob <jacob.heft@gmail.com>
2024-11-23 13:19:49 -0500

    Stop ephemeral deferrals

--- sisyphish/Controllers/HomeController.cs
+++ sisyphish/Controllers/HomeController.cs
@@ -71,7 +71,7 @@ public class HomeController : ControllerBase
     {
         var response = new DeferredDiscordInteractionResponse();
         
-        await _discord.DeferResponse(interaction, isEphemeral: true);
+        await _discord.DeferResponse(interaction, isEphemeral: false);
             
         await _cloudTasks.CreateHttpPostTask($"{Config.PublicBaseUrl}/sisyphish/fish", interaction);
 
