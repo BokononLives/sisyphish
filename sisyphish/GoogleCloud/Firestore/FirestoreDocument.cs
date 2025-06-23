e5aba863ec8b690f73c847e6df226c2519320b21
Jacob <jacob.heft@gmail.com>
2025-06-22 23:42:13 -0400

    Fix FirestoreDocument.LastUpdated

--- sisyphish/GoogleCloud/Firestore/FirestoreDocument.cs
+++ sisyphish/GoogleCloud/Firestore/FirestoreDocument.cs
@@ -3,5 +3,5 @@ namespace sisyphish.GoogleCloud.Firestore;
 public abstract class FirestoreDocument
 {
     public string? Id { get; set; }
-    public DateTime? LastUpdated { get; set; }
+    public string? LastUpdated { get; set; }
 }
