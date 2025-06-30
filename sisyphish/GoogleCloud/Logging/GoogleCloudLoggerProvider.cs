0e499fc97d0847c84c6d60a80457b62de1c1bdca
Jacob <1340259+BokononLives@users.noreply.github.com>
2025-06-30 06:13:09 -0400

    Remove http logging from logger http client

--- sisyphish/GoogleCloud/Logging/GoogleCloudLoggerProvider.cs
+++ sisyphish/GoogleCloud/Logging/GoogleCloudLoggerProvider.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using sisyphish.GoogleCloud.Authentication;
 
 namespace sisyphish.GoogleCloud.Logging;
@@ -5,19 +6,17 @@ namespace sisyphish.GoogleCloud.Logging;
 public class GoogleCloudLoggerProvider : ILoggerProvider
 {
     private readonly IGoogleCloudAuthenticationService _authenticationService;
-    private readonly IServiceProvider _serviceProvider;
+    private readonly IHttpClientFactory _httpClientFactory;
 
-    public GoogleCloudLoggerProvider(IGoogleCloudAuthenticationService authenticationService, IServiceProvider serviceProvider)
+    public GoogleCloudLoggerProvider(IGoogleCloudAuthenticationService authenticationService, IHttpClientFactory httpClientFactory)
     {
         _authenticationService = authenticationService;
-        _serviceProvider = serviceProvider;
+        _httpClientFactory = httpClientFactory;
     }
 
     public ILogger CreateLogger(string categoryName)
     {
-        var httpClientFactory = _serviceProvider.GetRequiredService<IHttpClientFactory>();
-        var httpClient = httpClientFactory.CreateClient(nameof(GoogleCloudLoggerProvider));
-        
+        var httpClient = _httpClientFactory.CreateClient(nameof(GoogleCloudLoggerProvider));
         return new GoogleCloudLoggingService(categoryName, _authenticationService, httpClient);
     }
 
